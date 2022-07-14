c6e8955f4d67716603a629ebcbe8677a79ff73bd
Brian Duong <brianminhduong@gmail.com>
2022-07-13 22:18:59 -0400

    Refactor useCounter to handle adding/subtracting

--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
 
-const useCounter = () => {
+const useCounter = (counterType) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      if (counterType === "up") {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
     }, 1000);
-
     return () => clearInterval(interval);
   }, []);
 
